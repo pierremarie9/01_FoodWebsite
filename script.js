641d56be513e18faf0610f1175a1197c564628b1
pierremarie9 <63357279+pierremarie9@users.noreply.github.com>
2020-06-09 14:25:57 +0200

    animations
    adding animations on scrolling

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    /*For the sticky navigation*/
     $('.js--section-features').waypoint(function(direction) {
         if(direction=='down'){
             $('nav').addClass('sticky');
@@ -10,12 +11,38 @@ $(document).ready(function() {
         offset: '60px'
       });
 
-    /*
-    var waypoints = $('#handler-first').waypoint(function(direction) {
-  notify(this.element.id + ' hit 25% from top of window') 
-}, {
-  offset: '25%'
-})
-    */
+      /*Scroll on buttons*/
+      $('.js--scroll-to-plans').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+      });
 
-})
+      $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+      });
+
+      /*Animations on scroll*/
+      $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+      }, {
+        offset: '50%'
+      });
+
+      $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+      }, {
+        offset: '50%'
+      });
+
+      $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+      }, {
+        offset: '50%'
+      });
+
+      $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
+      }, {
+        offset: '50%'
+      });
+
+});
